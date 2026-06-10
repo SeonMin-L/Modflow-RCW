89104e5c2359761cbde2556039dd6fc5b8fb3387
SeonMin-L <141636941+SeonMin-L@users.noreply.github.com>
2026-06-10 10:28:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import sys
 rw_path = ""  # Input file paths for Radial_Well.py and Gis_FloPy_nom.py have been set
 sys.path.append(rw_path)
+import flopy
 import Radial_Well as RW
-import Gis_FloPy_nom as gf
 
-model_setting_gis = "" # Input file path for the model_setting_gis
-Modflow_work_file = "" # Input file path for modflow workstation
-sim_name = "Test" # Input modflow name
+sim_name = "" # Input modflow name
+sim_path = "" # Input modflow folder path
+exe_path = "" # Input mf6.exe path
 
-sim = gf.modflow6_build(model_setting_gis, sim_name, Modflow_work_file)
-pipe_excel= "" # Input pipe xlsx file path
+sim = flopy.mf6.MFSimulation(sim_name=sim_name, version="mf6", exe_name="mf6.exe", sim_ws=sim_path)
 iterative_data, Cassion_input_data, sim, RCW_out = RW.run(sim, pipe_excel, sim_name)
 
 
