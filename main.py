e7028c5774f3ca22cf8f59e879f7416d0af6f007
SeonMin-L <141636941+SeonMin-L@users.noreply.github.com>
2026-07-27 14:03:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import Radial_Well as RW
 sim_name = "" # Input modflow name
 sim_path = "" # Input modflow folder path
 exe_path = "" # Input mf6.exe path
+pipe_excel = "" # Input pipe.excel path
 
 sim = flopy.mf6.MFSimulation(sim_name=sim_name, version="mf6", exe_name="mf6.exe", sim_ws=sim_path)
 iterative_data, Cassion_input_data, sim, RCW_out = RW.run(sim, pipe_excel, sim_name)
